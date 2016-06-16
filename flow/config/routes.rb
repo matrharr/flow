a4179b0ff780bc0081570217b2941c4f09ebad81
Matt Harris <matrharr@gmail.com>
2016-06-15 22:54:38 -0700

    add hidden tag to form

--- flow/config/routes.rb
+++ flow/config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
     resources :comments
   end
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+  root 'articles#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
