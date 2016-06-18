b4e77004ba196c08ec708c227f056e2e607fa8ac
Matt Harris <matrharr@gmail.com>
2016-06-18 14:53:11 -0700

    add password validation

--- flow/app/models/user.rb
+++ flow/app/models/user.rb
@@ -12,6 +12,8 @@ class User < ActiveRecord::Base
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
 
+
   has_secure_password
+  validates :password, presence: true, length: {minumum: 6}
 
 end
