b41a2b297bc29efd2d32f9268e0f4b26e17a055d
Matt Harris <matrharr@gmail.com>
2016-06-18 13:43:45 -0700

    add jquery & users

--- flow/config/routes.rb
+++ flow/config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   resources :articles do
     resources :comments
   end
+
+  resources :users
+  get '/login', to: 'users#login', as: :login
+  get '/signup', to: 'users#signup', as: :signup
   # You can have the root of your site routed with "root"
   root 'articles#index'
 
--- flow/db/schema.rb
+++ flow/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160615222520) do
+ActiveRecord::Schema.define(version: 20160618190011) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "author",     limit: 255
@@ -19,12 +19,22 @@ ActiveRecord::Schema.define(version: 20160615222520) do
     t.text     "body",       limit: 65535
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
+    t.integer  "user_id",    limit: 4
   end
 
   create_table "comments", force: :cascade do |t|
     t.integer  "article_id", limit: 4
     t.string   "author",     limit: 255
     t.string   "body",       limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+    t.integer  "user_id",    limit: 4
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.string   "email",      limit: 255
+    t.string   "password",   limit: 255
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
   end
