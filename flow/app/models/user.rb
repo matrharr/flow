861066abf2a8e25a313f6725a6b07fa83d2d65aa
Matt Harris <matrharr@gmail.com>
2016-06-18 14:15:57 -0700

    add user prescence/length validations and tests

--- flow/app/models/user.rb
+++ flow/app/models/user.rb
@@ -2,6 +2,10 @@ class User < ActiveRecord::Base
   has_many :articles
   has_many :comments
 
-  validates :name, presence:true
+  attr_accessor :name, :email
+
+  validates :name, presence:true, length: {maximum: 50}
+  validates :email, presence:true, length: {maximum: 255}
+
 
 end
