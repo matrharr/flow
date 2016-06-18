2f988a569530f70487159647425c98df8112991e
Matt Harris <matrharr@gmail.com>
2016-06-18 14:49:28 -0700

    add has secure password validations, columns and tests

--- flow/db/schema.rb
+++ flow/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160618190011) do
+ActiveRecord::Schema.define(version: 20160618214622) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "author",     limit: 255
@@ -32,11 +32,15 @@ ActiveRecord::Schema.define(version: 20160618190011) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "name",       limit: 255
-    t.string   "email",      limit: 255
-    t.string   "password",   limit: 255
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.string   "name",                  limit: 255
+    t.string   "email",                 limit: 255
+    t.string   "password",              limit: 255
+    t.datetime "created_at",                        null: false
+    t.datetime "updated_at",                        null: false
+    t.string   "password_digest",       limit: 255
+    t.string   "password_confirmation", limit: 255
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
 end
