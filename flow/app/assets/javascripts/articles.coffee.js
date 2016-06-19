82b0b5032ba6f5e39b799de9e533d280e00fb0de
Matt Harris <matrharr@gmail.com>
2016-06-19 11:29:58 -0700

    refactor jquery functions further

--- flow/app/assets/javascripts/articles.coffee.js
+++ flow/app/assets/javascripts/articles.coffee.js
@@ -44,45 +44,48 @@
 (function($, window, document){
 
   $(function(){
-    $('#new_article_link').on('click', showForm)
-    $('#new_article_container').on('submit', '#new_article', submitForm)
+    showForm();
+    submitForm();
   });
 
-  var showForm = function(e){
-  e.preventDefault()
-  var that = $(this)
-  var url = that.attr('href')
-
-  if(that.hasClass('open')){
-    that.next().remove()
-    that.toggleClass('open')
-    that.html('<p>Write a new post</p>')
-  }else{
-    that.html('<p>Close</p>')
-    that.toggleClass('open')
-    $.ajax({
-      url: url,
-      method: 'GET'
-    }).done(function(response){
-      that.after(response)
+  var showForm = function(){
+    $('#new_article_link').on('click', function(e){
+        e.preventDefault();
+        var that = $(this);
+        var url = that.attr('href')
+        if(that.hasClass('open')){
+          that.next().remove()
+          that.toggleClass('open')
+          that.html('<p>Write a new post</p>')
+        }else{
+          that.html('<p>Close</p>')
+          that.toggleClass('open')
+          $.ajax({
+            url: url,
+            method: 'GET'
+          }).done(function(response){
+            that.after(response)
+          })
+        }
     })
   }
-}
 
-  var submitForm = function(e){
-    e.preventDefault()
-    var that = $(this)
-    var data = that.serialize()
-    var url = that.attr('action')
-    $.ajax({
-      url: url,
-      data: data,
-      method: 'POST'
-    }).done(function(response){
-      that.parent().after(response)
-      that.parent().remove()
-      $('#new_article_link').html('<p>Write a new post</p>')
 
+  var submitForm = function(){
+    $('#new_article_container').on('submit', '#new_article', function(e){
+        e.preventDefault()
+        var that = $(this)
+        var data = that.serialize()
+        var url = that.attr('action')
+        $.ajax({
+          url: url,
+          data: data,
+          method: 'POST'
+        }).done(function(response){
+          that.parent().after(response)
+          that.parent().remove()
+          $('#new_article_link').html('<p>Write a new post</p>')
+        })
     })
   }
 
